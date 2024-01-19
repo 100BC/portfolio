ebc75e362757733627a076aecd70ec3ad125edc5
100BC <73491141+100BC@users.noreply.github.com>
2024-01-19 16:42:58 -0500

    Add Configuration Files and Packages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,31 +1,47 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
-	root: true,
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:svelte/recommended',
-		'prettier'
-	],
-	parser: '@typescript-eslint/parser',
-	plugins: ['@typescript-eslint'],
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	},
-	overrides: [
-		{
-			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser',
-			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:svelte/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+    ecmaVersion: 2020,
+    extraFileExtensions: ['.svelte'],
+  },
+  env: {
+    browser: true,
+    es2017: true,
+    node: true,
+  },
+  overrides: [
+    {
+      files: ['*.svelte'],
+      parser: 'svelte-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  ],
+  rules: {
+    // 'import/no-unresolved': [
+    //   2,
+    //   { ignore: ['^\\$lib', '^@sveltejs', '^\\$app', '^\\$env'] },
+    // ],
+    'import/no-extraneous-dependencies': 'off',
+    'svelte/block-lang': ['error', { script: 'ts', style: 'scss' }],
+    'svelte/button-has-type': 'warn',
+    'svelte/no-ignored-unsubscribe': 'error',
+    'svelte/no-useless-mustaches': 'warn',
+    'import/no-mutable-exports': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off',
+    'no-undef-init': 'off',
+  },
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: "@use 'src/lib/theme' as *;",
+      },
+      postcss: {
+        map: true,
+        plugins: [autoprefixer()],
+      },
+    },
+  },
 });
