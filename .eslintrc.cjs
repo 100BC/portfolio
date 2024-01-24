97193a6ed820dbc859083008e56e395863e662b6
100BC <73491141+100BC@users.noreply.github.com>
2024-01-23 22:33:19 -0500

    Add comments

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -30,18 +30,19 @@ module.exports = {
     },
   ],
   rules: {
-    // 'import/no-unresolved': [
-    //   2,
-    //   { ignore: ['^\\$lib', '^@sveltejs', '^\\$app', '^\\$env'] },
-    // ],
-    'import/no-extraneous-dependencies': 'off',
+    // Svelte
     'svelte/block-lang': ['error', { script: 'ts', style: 'scss' }],
     'svelte/button-has-type': 'warn',
     'svelte/no-ignored-unsubscribe': 'error',
     'svelte/no-useless-mustaches': 'warn',
+
+    // Svelte Workarounds
+    'import/no-extraneous-dependencies': 'off',
     'import/no-mutable-exports': 'off',
     'import/prefer-default-export': 'off',
     'import/extensions': 'off',
+
+    // Pref TypeScript
     'no-undef-init': 'off',
     'no-unused-vars': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
